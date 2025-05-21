89b749e4cc2a5777138d656541f2f26a25b4daca
Blessing <blessingogbogu@gmail.com>
2025-05-21 17:45:58 +0000

    Updating vite.config file

--- src/app.py
+++ src/app.py
@@ -4,11 +4,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
-from src.api.utils import APIException, generate_sitemap
-from src.api.models import db
-from src.api.routes import api
-from src.api.admin import setup_admin
-from src.api.commands import setup_commands
+from api.utils import APIException, generate_sitemap
+from api.models import db
+from api.routes import api
+from api.admin import setup_admin
+from api.commands import setup_commands
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_bcrypt import Bcrypt
 from datetime import timedelta
